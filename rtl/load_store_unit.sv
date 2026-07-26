c5be496b4d73dac9dc42cc105a71ae3642e05f05
Influence631 <kdmitro80@gmail.com>
2026-07-26 17:55:07 +0100

    Branch comparator unit RTL

--- rtl/load_store_unit.sv
+++ rtl/load_store_unit.sv
@@ -1,5 +1,7 @@
 `default_nettype none
 
+//i still need to figure out the semantics for this one,
+//but it will handle the readings from memory writings to regfile 
 module load_store_unit (
   input wire logic [31:0] alu_res_i,
   input wire logic mem_write_i,
